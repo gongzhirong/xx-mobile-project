82b6a360e7514da0f6440449c1f4774443c4f8b3
gongzhirong <gongzhirong@chinasie.com>
2019-07-31 14:31:35 +0800

    删除ssh测试代码

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import * as serviceWorker from './serviceWorker';
 import Router from './router.js'
 
 // 测试SourceTree
-// 测试本地ssh
 ReactDOM.render(<Router /> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
